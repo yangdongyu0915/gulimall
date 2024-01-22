bdcd46993245fbd32cec31ab7e67e9c4ecf16b76
yangdy <1752510119@qq.com>
2024-01-22 10:33:12 +0800

    sss

---
+++ src/main/java/com/ydy/gulimalll/GulimalllApplication.java
@@ -0,0 +1,13 @@
+package com.ydy.gulimalll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimalllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimalllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ydy/gulimalll/GulimalllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydy.gulimalll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimalllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
